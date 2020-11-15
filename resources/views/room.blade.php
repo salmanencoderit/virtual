20bed73367d5d484cf18c3061247f87c7a812041
Transferista <office@transferista.com>
2020-11-15 21:48:44 +0100

    Title

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -1,11 +1,14 @@
 <!doctype html>
+@extends('layouts.app')
+
+@section('content')
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Virtual Wedding Expo</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
