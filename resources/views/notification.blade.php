306203d72ff12572064472eca5d8c879b22ce4cf
Transferista <office@transferista.com>
2020-11-15 16:48:04 +0100

    notification

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -5,16 +5,21 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Notification - User wurde angelegt') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                Ihre E-Mail Adresse für das Login: {{ $email }}<br>
+                Ihr Passwort (Case-sensitive):     {{ $password }}<br>
+                <br>
+                <hr>
+                <br>
+                Ihr Namen: {{ $name }} {{ $lastname }}<br>
+                Ihre Firma: {{ $company }} - Branche: {{ $sector }}
+                <br><br>
+                Sie können gleich nach der Messe mit Ihren Kunden über diese Seite kommunizieren!<br>
+                <br>
+
                 </div>
             </div>
         </div>
