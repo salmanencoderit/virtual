f5ab4b313a612ad6a4cf1d89d1b573089813a87e
Transferista <office@transferista.com>
2020-11-15 17:08:38 +0100

    Update notification.blade.php

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -5,20 +5,23 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Notification - User wurde angelegt') }}</div>
+                <div class="card-header">{{ __('Notification - User wurde angelegt - Zugangsdaten') }}</div>
 
                 <div class="card-body">
-
-                Ihre E-Mail Adresse für das Login: {{ $email }}<br>
-                Ihr Passwort (Case-sensitive):     {{ $password }}<br>
-                <br>
-                <hr>
-                <br>
-                Ihr Namen: {{ $name }} {{ $lastname }}<br>
-                Ihre Firma: {{ $company }} - Branche: {{ $sector }}
-                <br><br>
-                Sie können gleich nach der Messe mit Ihren Kunden über diese Seite kommunizieren!<br>
-                <br>
+                  <br>
+                  Vielen Dank für Ihre Registrierung zur Virtual Wedding Expo!<br><br>
+                  Diese Website soll Ihnen helfen - nach der Hochzeit - mit den Besuchern in Kontakt<br>
+                  zu treten. Sie finden unten stehend Ihre LoginDaten:<br><br>
+                  Ihre E-Mail Adresse für das Login: {{ $email }}<br>
+                  Ihr Passwort (Case-sensitive):     {{ $password }}<br>
+                  <br>
+                  <hr>
+                  <br>
+                  Ihr Namen: {{ $name }} {{ $lastname }}<br>
+                  Ihre Firma: {{ $company }} - Branche: {{ $sector }}
+                  <br><br>
+                  Sie können gleich nach der Messe mit Ihren Kunden über diese Seite virtuel in Kontakt treten!<br>
+                  <br>
 
                 </div>
             </div>
