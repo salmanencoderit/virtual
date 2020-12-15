d63a828bb1292add244ef1710154b9e00bfc2e0e
Transferista <office@transferista.com>
2020-12-15 22:40:40 +0100

    Update notification.blade.php

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -12,18 +12,18 @@
                   Vielen Dank für Ihre Registrierung zur Virtual Wedding Expo!<br><br>
                   Diese Website soll Ihnen helfen - nach der Hochzeitsmesse - mit den Besuchern in Kontakt<br>
                   zu treten. Sie finden unten stehend Ihre LoginDaten:<br><br>
-                  Ihre E-Mail Adresse für das Login: {{ $email }}<br>
-                  Ihr Passwort (Case-sensitive):     {{ $password }}<br>
+                  Ihre E-Mail Adresse für das Login: {{ email }}<br>
+                  Ihr Passwort (Case-sensitive):     {{ password }}<br>
                   <br>
                   <hr>
                   <br>
-                  Ihr Namen: {{ $firstname }} {{ $lastname }}<br>
-                  @if ($role === 1) {
+                  Ihr Namen: {{ firstname }} {{ lastname }}<br>
+                  @if (role === 1) {
 
                     Rolle: Aussteller<br>
 
                   }@endif
-                  Ihre Firma: {{ $company }} - Branche: {{ $sector }}
+                  Ihre Firma: {{ company }} - Branche: {{ sector }}
                   <br><br>
                   Sie können gleich nach der Messe mit Ihren Kunden über diese Seite virtuel in Kontakt treten!<br>
                   <br>
