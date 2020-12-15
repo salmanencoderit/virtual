941d9e622eb157795bde171257df562759a50d27
Transferista <office@transferista.com>
2020-12-15 23:18:49 +0100

    Update notification.blade.php

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -13,16 +13,16 @@
                   Diese Website soll Ihnen helfen - nach der Hochzeitsmesse - mit den Besuchern in Kontakt<br>
                   zu treten. Sie finden unten stehend Ihre LoginDaten:<br><br>
                   Ihre E-Mail Adresse für das Login: {{ $user->email }}<br>
-                  Ihr Passwort (Case-sensitive):     {{ $user->password }}<br>
+                  Ihr Passwort (Case-sensitive):     {{ $password }}<br>
                   <br>
                   <hr>
                   <br>
                   Ihr Namen: {{ $user->name }} {{ $user->lastname }}<br>
-                  @if ($user->role === 1) {
+                  @if ($user->role === 1)
 
-                    Rolle: Aussteller<br>
+                  Rolle: Aussteller<br>
 
-                  }@endif
+                  @endif
                   Ihre Firma: {{ $user->company }} - Branche: {{ $user->sector }}
                   <br><br>
                   Sie können gleich nach der Messe mit Ihren Kunden über diese Seite virtuel in Kontakt treten!<br>
