60666cd9452c2f885f4d13d9a479e584d0d78ebd
Transferista <office@transferista.com>
2020-12-16 11:18:00 +0100

    Logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                  <a href="{{ url('/home') }}" class="navbar-left"><img src="{{ asset('LogoLG2020@2x.png') }}"></a>
+                  <a href="{{ url('/home') }}" class="navbar-left"><img src="{{ asset('Logo.png') }}"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
